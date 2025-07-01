22edebe2989f7c202c2fe7f356ce7e6b1235a11b
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-07-01 11:34:59 +0200

    setting express

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,18 @@ import express from "express";
 const PORT = 3333;
 
 const app = express();
+app.use(express.json());
+
+app.get("/products", (request, response) => {
+  const { page, limit } = request.query;
+  response.send(`Page ${page} de ${limit} `);
+});
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body;
+
+  //   response.send(`Product ${name} cost $ ${price}`);
+  response.status(201).json({ name, price });
+});
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
