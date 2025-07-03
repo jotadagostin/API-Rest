39ae24fc718cd6b92d2016d90e8b540421222d91
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-07-03 10:48:36 +0200

    zod

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { AppError } from "../utils/AppError";
+import { z } from "zod";
 
 class ProductsController {
   //* index - get para listar varios registros
@@ -15,23 +16,29 @@ class ProductsController {
   }
 
   create(request: Request, response: Response) {
-    const { name, price } = request.body;
+    const bodySchema = z.object({
+      name: z
+        .string({ required_error: "Name is required" })
+        .min(6, { message: "Name must be 6 or more characters" }),
+      price: z.number({ required_error: "Price is required" }),
+    });
 
-    if (!name) {
-      throw new AppError("Name is required!");
-    }
+    const { name, price } = bodySchema.parse(request.body);
+    // if (!name) {
+    //   throw new AppError("Name is required!");
+    // }
 
-    if (name.trim().length < 6) {
-      throw new AppError("Name has to long enought(6 caracters)");
-    }
+    // if (name.trim().length < 6) {
+    //   throw new AppError("Name has to long enought(6 caracters)");
+    // }
 
-    if (!price) {
-      throw new AppError("Price is required!");
-    }
+    // if (!price) {
+    //   throw new AppError("Price is required!");
+    // }
 
-    if (price < 0) {
-      throw new AppError("Price can not be negative");
-    }
+    // if (price < 0) {
+    //   throw new AppError("Price can not be negative");
+    // }
 
     // throw new Error("ERRO AO TENTAR CRIAR UM PRODUTO!");
     // throw new AppError("Error when tried to creat the product!");
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { routes } from "./routes";
 import { AppError } from "./utils/AppError";
+import { ZodError } from "zod";
 
 const PORT = 3333;
 
@@ -13,6 +14,12 @@ app.use(routes);
 app.use((error: any, request: Request, response: Response, _: NextFunction) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    response
+      .status(400)
+      .json({ message: "Validation error!", issues: error.format() });
   }
   response.status(500).json({ message: error.message });
 });
