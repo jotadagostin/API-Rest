b6ea9f8abb99871cfa7a00c47b48f9af4dad68a6
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-07-03 10:56:35 +0200

    gitignore

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -19,8 +19,11 @@ class ProductsController {
     const bodySchema = z.object({
       name: z
         .string({ required_error: "Name is required" })
+        .trim()
         .min(6, { message: "Name must be 6 or more characters" }),
-      price: z.number({ required_error: "Price is required" }),
+      price: z
+        .number({ required_error: "Price is required" })
+        .positive({ message: "Price must be positive" }),
     });
 
     const { name, price } = bodySchema.parse(request.body);
